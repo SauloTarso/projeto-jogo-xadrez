9e4d297461a3ad668f9b2741e17c4e63b1b11259
SauloTarso <saulotlamorim@gmail.com>
2023-08-23 00:06:12 -0300

    Camada de xadrez e impressão do tabuleiro

--- src/aplicacao/Program.java
+++ src/aplicacao/Program.java
@@ -1,12 +1,13 @@
 package aplicacao;
 
-import TabuleiroDoJogo.Tabuleiro;
+import xadrez.PartidaDeXadrez;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+		PartidaDeXadrez partidaDeXadrez = new PartidaDeXadrez();
 		
+		UI.imprimirTabuleiro(partidaDeXadrez.getPecas());
 	}
 }
